1f2b4917daecdabcfcf2bd77149fe7dbfe9e5ab7
Luis Luna <luisluna@Luiss-MacBook-Pro.local>
2014-04-29 21:08:36 -0700

    New User registration is finished. Now to work on the rest of the app

--- loginFrame.java
+++ loginFrame.java
@@ -22,7 +22,6 @@ public class loginFrame extends javax.swing.JFrame {
         initComponents();
     }
 
-    
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
